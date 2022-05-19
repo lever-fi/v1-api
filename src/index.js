a4e5b06427d190063fd233ebcb68029ba8b7a556
prathik <37804760+prmali@users.noreply.github.com>
2022-05-18 19:22:58 -0500

    Init commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const router = express.Router();
+
+router.get("/volume/", async (req, res) => {
+	try {
+		const { chain, address, time } = req.query;
+
+		res.status(200).send({
+			volume: 50.795,
+		});
+	} catch (err) {
+		return res.status(400).send(err?.message || error);
+	}
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(router);
+
+app.listen(3000, () => {
+	console.log("Server live");
+});
